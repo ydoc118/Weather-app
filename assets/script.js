21cdd1171704a46d3c047ac41a8c9c63d35c8452
Cody <ydoc118@gmail.com>
2020-07-02 23:13:40 -0400

    fixed 5 day forecast on saved cities

--- assets/script.js
+++ assets/script.js
@@ -53,6 +53,54 @@ $(document).ready(function() {
                     $("#temp").text("Temperature (F): " + tempF.toFixed(2));
                     $("#humidity").text("Humidity: " + cityHumidity);
                     $("#wind-speed").text("Wind Speed: " + cityWind);
+
+
+                    $.ajax({
+                        url: forecastURL,
+                        method: "GET"
+                    }).then(function(response) {
+                        console.log(response);
+            
+                        $("#five-day").css("display", "inherit")
+                        var fiveDayIcon = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
+                        var fiveDayIcon2 = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
+                        var fiveDayIcon3 = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
+                        var fiveDayIcon4 = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
+                        var fiveDayIcon5 = $("<img>").attr("src", "http://openweathermap.org/img/w/" + response.list[0].weather[0].icon + ".png");
+                        
+                            var fiveDayForecast = [
+                                {date: moment().add(1, 'days').calendar(),
+                                temp: "Temp(F): " + ((response.list[0].temp.day - 273.15) * 1.80 + 32).toFixed(2),
+                                humidity: "Humidity: " + response.list[0].humidity + "%"},
+            
+                                {date: moment().add(2, 'days').calendar(),
+                                temp: "Temp(F): " + ((response.list[1].temp.day - 273.15) * 1.80 + 32).toFixed(2),
+                                humidity: "Humidity: " + response.list[1].humidity + "%"},
+            
+                                {date: moment().add(3, 'days').calendar(),
+                                temp: "Temp(F): " + ((response.list[2].temp.day - 273.15) * 1.80 + 32).toFixed(2),
+                                humidity: "Humidity: " + response.list[2].humidity + "%"},
+            
+                                {date: moment().add(4, 'days').calendar(),
+                                temp: "Temp(F): " + ((response.list[3].temp.day - 273.15) * 1.80 + 32).toFixed(2),
+                                humidity: "Humidity: " + response.list[3].humidity + "%"},
+            
+                                {date: moment().add(5, 'days').calendar(),
+                                temp: "Temp(F): " + ((response.list[4].temp.day - 273.15) * 1.80 + 32).toFixed(2),
+                                humidity: "Humidity: " + response.list[4].humidity + "%"}
+                            ]
+                        
+                                $("#forecast-list").html("5 Day Forecast: ")
+                                $("#1").html(fiveDayForecast[0].date + "<br>" + fiveDayForecast[0].temp + "<br>" + fiveDayForecast[0].humidity).append(fiveDayIcon)
+                                $("#2").html(fiveDayForecast[1].date + "<br>" + fiveDayForecast[1].temp + "<br>" + fiveDayForecast[1].humidity).append(fiveDayIcon2)
+                                $("#3").html(fiveDayForecast[2].date + "<br>" + fiveDayForecast[2].temp + "<br>" + fiveDayForecast[2].humidity).append(fiveDayIcon3)
+                                $("#4").html(fiveDayForecast[3].date + "<br>" + fiveDayForecast[3].temp + "<br>" + fiveDayForecast[3].humidity).append(fiveDayIcon4)
+                                $("#5").html(fiveDayForecast[4].date + "<br>" + fiveDayForecast[4].temp + "<br>" + fiveDayForecast[4].humidity).append(fiveDayIcon5)
+            
+                            
+                    })
+            
+                    
                 })
             })
 
